4726e5e1d2f4d3c88c23d0cbd9adcb2b55268ff9
Bruno Avila <bruno@hotelsaoluiz.com>
2026-05-30 20:35:12 -0300

    fix: define formatBytes helper function in Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -343,6 +343,15 @@ export function Admin() {
     }
   }
 
+  const formatBytes = (bytes, decimals = 2) => {
+    if (bytes === 0) return '0 Bytes'
+    const k = 1024
+    const dm = decimals < 0 ? 0 : decimals
+    const sizes = ['Bytes', 'KB', 'MB', 'GB']
+    const i = Math.floor(Math.log(bytes) / Math.log(k))
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
+  }
+
   const formatBrl = (price) => {
     return new Intl.NumberFormat('pt-BR', {
       style: 'currency',
